3b0308e78ab989639e21d6b678d8e21d03ec5aa4
hardikbeladiya <beladiyahardik7@gmail.com>
2019-10-15 13:37:26 +0530

    Added the 100% test case for the Email Service with jest.

--- src/tests/email.test.ts
+++ src/tests/email.test.ts
@@ -1,22 +1,29 @@
-import AWS = require('aws-sdk');
-import * as sinon from 'sinon';
 import { Email } from '../components/email';
 
-let sinonSandbox: sinon.SinonSandbox;
-let promise: sinon.SinonStub;
+let promise: jest.Mock<any, any>;
+
+const sendEmail = jest.fn();
+jest.mock('aws-sdk', () => {
+    return {
+        SES: jest.fn(() => ({sendEmail})),
+        config: {
+            update: jest.fn(() => {}),
+        },
+    };
+});
 
 describe('## Email Component', () => {
     describe(`# Send emails via AWS SES service`, () => {
 
         beforeAll(() => {
-            sinonSandbox = sinon.createSandbox();
-            promise = sinon.stub().returns({MessageId: 'MessageId'});
-            const aws = sinonSandbox.stub(AWS, 'SES').withArgs(sinon.match.object);
-            aws.returns({ sendEmail: () => ({promise})});
+            promise = jest.fn().mockReturnValue({MessageId: 'MessageId'});
+            sendEmail.mockImplementation((params) => {
+                return {promise};
+            });
         });
 
         afterAll( () => {
-            sinonSandbox.restore();
+            jest.clearAllMocks();
         });
 
         it('should validate that the `confirmEmail` send an email and return 200 ok', async (done) => {
@@ -32,14 +39,14 @@ describe('## Email Component', () => {
         });
 
         it('should throw the Error with `confirmEmail` and console Authentication required', async (done) => {
-            promise = sinon.stub().throwsException({code: 530, cfId: 12, message: 'Authentication required'});
+            promise = jest.fn().mockRejectedValue({code: 530, cfId: 12, message: 'Authentication required'});
             const res = await Email.confirmEmail('fullname', 'test8@dicky.world', 'confirmationcode');
             expect(res).toBe(false);
             done();
         });
 
         it('should throw the Error with `resetPassword` and console Temporary service failure', async (done) => {
-            promise = sinon.stub().throwsException({code: 451, cfId: 12, message: 'Temporary service failure'});
+            promise = jest.fn().mockRejectedValue({code: 451, cfId: 12, message: 'Temporary service failure'});
             const res = await Email.resetPassword('fullname', 'test8@dicky.world', 'resetcode');
             expect(res).toBe(false);
             done();
