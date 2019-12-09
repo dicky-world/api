0e59a970911c2ea477fe865b87b518a29d5044de
bkawk <email.will.in.china@gmail.com>
2019-12-09 21:13:00 +0800

    remove tests

--- src/tests/app.ts
+++
@@ -1,9 +0,0 @@
-
-import { startMongo } from './mongo';
-
-export const mockApp = startMongo.then((url) => {
-     process.env.API_PORT = '0';
-     process.env.JWT_SECRET = 'secret';
-     process.env.EMAIL_FROM = 'test8@dicky.world';
-     return require('../server').app;
-});
